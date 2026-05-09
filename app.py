23c9e067fbff331c718ca6150f3da2e375e8d010
swapnil.analyst <swapnilbramhankar14oct@gmail.com>
2026-05-09 09:55:43 +0530

    fix deployment issue - added port handling

--- app.py
+++ app.py
@@ -146,4 +146,4 @@ def delete(image_id):
     return redirect(request.referrer)
 
 if __name__ == '__main__':
-        app.run(debug=True)
+        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
